736b75e1501bdd5481ddf05f86a30ce0e06612d9
quynhngo2909 <dieuquynh105@gmail.com>
2023-05-06 16:05:21 +0700

    add fe be

--- rebnb-fe/src/app/reservation/reservation-wishlist/reservation-wishlist.component.ts
+++ rebnb-fe/src/app/reservation/reservation-wishlist/reservation-wishlist.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {BookingService} from '../../service/booking.service';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -13,31 +13,31 @@ import {render} from 'creditcardpayments/creditCardPayments';
 export class ReservationWishlistComponent implements OnInit {
   userId: number;
   unpaidBookingList: Booking[];
+  unpaidBooking1: Booking;
+  unpaidBooking2: Booking;
+  unpaidBooking3: Booking;
+  unpaidBooking4: Booking;
+  isBooking1Paid = false;
+  isBooking2Paid = false;
+  isBooking3Paid = false;
+  isBooking4Paid = false;
   mess = '';
   page = 0;
   totalPages: number;
+  isCalledPayPal = false;
 
   constructor(private bookingService: BookingService,
               private http: HttpClient,
               private activatedRoute: ActivatedRoute,
-              private route: Router) {
-    render(
-      {
-        id: '#myPayPalButton',
-        currency: 'USD',
-        value: '100.00',
-        onApprove: (details) => {
-          alert('Transaction successful');
-        }
-      }
-      );
+              private route: Router,
+              private elementRef: ElementRef) {
   }
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(params => {
-      this.userId = +params.get('userId');
+    this.activatedRoute.paramMap.subscribe(async params => {
+      this.userId = await +params.get('userId');
+      await this.getUnpaidBooking();
     });
-    this.getUnpaidBooking();
   }
 
   getUnpaidBooking() {
@@ -50,6 +50,10 @@ export class ReservationWishlistComponent implements OnInit {
 
       if (items !== null) {
         this.unpaidBookingList = items.content;
+        this.unpaidBooking1 = this.unpaidBookingList[0];
+        this.unpaidBooking2 = this.unpaidBookingList[1];
+        this.unpaidBooking3 = this.unpaidBookingList[2];
+        this.unpaidBooking4 = this.unpaidBookingList[3];
         this.totalPages = items.totalPages;
       }
     }, () => {
@@ -78,4 +82,68 @@ export class ReservationWishlistComponent implements OnInit {
     this.getUnpaidBooking();
   }
 
+
+  getPayPal() {
+    this.isCalledPayPal = !this.isCalledPayPal;
+    this.getPayPal1();
+    this.getPayPall2();
+    this.getPayPall3();
+    this.getPayPall4();
+  }
+
+  getPayPal1() {
+    render(
+      {
+        id: '#paypal-button-container-unpaidBooking1',
+        currency: 'USD',
+        value: this.unpaidBooking1.totalPrice.toFixed(),
+        onApprove: (details) => {
+          alert('Transaction successful');
+          this.isBooking1Paid = true;
+        }
+      }
+    );
+  }
+
+  getPayPall2() {
+    render(
+      {
+        id: '#paypal-button-container-unpaidBooking2',
+        currency: 'USD',
+        value: this.unpaidBooking2.totalPrice.toFixed(),
+        onApprove: (details) => {
+          alert('Transaction successful');
+          this.isBooking2Paid = true;
+        }
+      }
+    );
+  }
+
+  getPayPall3() {
+    render(
+      {
+        id: '#paypal-button-container-unpaidBooking3',
+        currency: 'USD',
+        value: this.unpaidBooking3.totalPrice.toFixed(),
+        onApprove: (details) => {
+          alert('Transaction successful');
+          this.isBooking3Paid = true;
+        }
+      }
+    );
+  }
+
+  getPayPall4() {
+    render(
+      {
+        id: '#paypal-button-container-unpaidBooking4',
+        currency: 'USD',
+        value: this.unpaidBooking4.totalPrice.toFixed(),
+        onApprove: (details) => {
+          alert('Transaction successful');
+          this.isBooking4Paid = true;
+        }
+      }
+    );
+  }
 }
